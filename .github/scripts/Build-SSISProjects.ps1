d1d6490ed2cf0f8ccc74b015c6c909f51eb4c3a8
Drew Furgiuele <drewfurgiuele@gmail.com>
2021-03-09 15:40:39 -0500

    Fixed command execution

--- .github/scripts/Build-SSISProjects.ps1
+++ .github/scripts/Build-SSISProjects.ps1
@@ -2,15 +2,11 @@ $Workspace = $env:GITHUB_WORKSPACE
 $Solution = Get-ChildItem -Path "$Workspace\*"  | Where-Object {$_.Extension -eq ".sln"}
 $Projects = Get-ChildItem -Path "$Workspace\*\*"  | Where-Object {$_.Extension -eq ".dtproj"}
 
-Write-Host $Workspace
-
-
 foreach ($p in $Projects) {
     $SolutionFulleName = $Solution.FullName
     $ProjectFullName = $p.fullname
-    Write-Host $ProjectFullName
     $Command = "`"C:\Program Files (x86)\Microsoft Visual Studio\2017\SQL\Common7\IDE\devenv.exe`" `"$SolutionFulleName`" /ReBuild Development /Project `"$ProjectFullName`""
-    Write-Host $Command
-    Start-Process -FilePath $Command -Wait
+
+    & $Command
     
 }
